df53e680f4157981e837a6b0a6b384d4ee32e7c5
adityaraj079 <55791929+adityaraj079@users.noreply.github.com>
2020-05-15 20:05:41 +0530

    Create Q_17.cpp

---
+++ Q_17.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int eng,chem,ads,math;
+    while(1){
+    try {
+  int eng,chem,ads,math;
+  cout<<"Marks in english"<<endl;
+  cin>>eng;
+  cout<<"Marks in chemistry"<<endl;
+  cin>>chem;
+  cout<<"Marks in ads"<<endl;
+  cin>>ads;
+  cout<<"Marks in math"<<endl;
+  cin>>math;
+  if (eng,chem,ads,math > 0 && eng,chem,ads,math < 100 ) {
+    cout << "-  _  - CONGO.";
+    return 0;
+  } else {
+    throw (eng,chem,ads,math);
+  }
+}
+catch (...) {
+  cout << "Invalid Marks.\n";
+ 
+}
+if (eng,chem,ads,math < 0 && eng,chem,ads,math > 100 )
+  break;
+}
+}
